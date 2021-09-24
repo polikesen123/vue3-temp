59ec5b0a2affbf2d55da14a96527e9d6c6051502
xiaopf <xiaopf@syc.green>
2021-09-24 12:26:57 +0800

    feat: unplugin-vue-components按需加载antdv@next和element-plus,暂不支持按需加载组件样式

--- src/main.js
+++ src/main.js
@@ -2,5 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'element-plus/dist/index.css'
+// import 'element-plus/lib/theme-chalk/base.css'
+import installElementPlus from './element'
+let app = createApp(App);
+installElementPlus(app);
+import 'ant-design-vue/dist/antd.css';
+import installAntdv from './antdv';
+installAntdv(app);
 
-createApp(App).use(store).use(router).mount('#app')
+app.use(store).use(router).mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,83 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <HelloWorld msg="Welcome to Your Vue.js App" />
+        <a-menu
+            id="dddddd"
+            style="width: 256px"
+            v-model:openKeys="openKeys"
+            v-model:selectedKeys="selectedKeys"
+            mode="inline"
+            @click="handleClick"
+        >
+            <a-sub-menu key="sub1" @titleClick="titleClick">
+                <template #icon> 🐱 </template>
+                <template #title>Navigation One</template>
+                <a-menu-item-group key="g1">
+                    <template #icon> 🐱 </template>
+                    <template #title>Item 1</template>
+                    <a-menu-item key="1">Option 1</a-menu-item>
+                    <a-menu-item key="2">Option 2</a-menu-item>
+                </a-menu-item-group>
+                <a-menu-item-group key="g2" title="Item 2">
+                    <a-menu-item key="3">Option 3</a-menu-item>
+                    <a-menu-item key="4">Option 4</a-menu-item>
+                </a-menu-item-group>
+            </a-sub-menu>
+            <a-sub-menu key="sub2" @titleClick="titleClick">
+                <template #icon> 🐱 </template>
+                <template #title>Navigation Two</template>
+                <a-menu-item key="5">Option 5</a-menu-item>
+                <a-menu-item key="6">Option 6</a-menu-item>
+                <a-sub-menu key="sub3" title="Submenu">
+                    <a-menu-item key="7">Option 7</a-menu-item>
+                    <a-menu-item key="8">Option 8</a-menu-item>
+                </a-sub-menu>
+            </a-sub-menu>
+            <a-sub-menu key="sub4">
+                <template #icon> 🐱 </template>
+                <template #title>Navigation Three</template>
+                <a-menu-item key="9">Option 9</a-menu-item>
+                <a-menu-item key="10">Option 10</a-menu-item>
+                <a-menu-item key="11">Option 11</a-menu-item>
+                <a-menu-item key="12">Option 12</a-menu-item>
+            </a-sub-menu>
+        </a-menu>
+    </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
+import { defineComponent, ref, watch } from "vue";
+export default defineComponent({
+    name: "Home",
+    setup() {
+        const selectedKeys = ref(["1"]);
+        const openKeys = ref(["sub1"]);
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+        const handleClick = (e) => {
+            console.log("click", e);
+        };
+
+        const titleClick = (e) => {
+            console.log("titleClick", e);
+        };
+
+        watch(
+            () => openKeys,
+            (val) => {
+                console.log("openKeys", val);
+            }
+        );
+        return {
+            selectedKeys,
+            openKeys,
+            handleClick,
+            titleClick,
+        };
+    },
+    components: {
+        HelloWorld,
+    },
+});
 </script>
